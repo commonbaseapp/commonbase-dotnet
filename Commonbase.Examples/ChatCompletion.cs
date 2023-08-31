1eb431487ed4d95e350317a4840aa002af7aa616
Brandon Meyerowitz <brandon@commonbase.com>
2023-08-31 15:16:38 -0400

    small change

--- Commonbase.Examples/ChatCompletion.cs
+++ Commonbase.Examples/ChatCompletion.cs
@@ -21,8 +21,7 @@ public static class ChatCompletionExample
     Console.WriteLine("\nResponse:");
 
     var response = await client.CreateChatCompletionAsync(
-      messages: messages,
-      providerConfig: new CbOpenAIProviderConfig(CbOpenAIRegion.EU)
+      messages: messages
     );
 
     Console.WriteLine(response.BestChoice.Text);
